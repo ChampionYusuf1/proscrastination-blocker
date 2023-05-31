1969a1683c35a9ed204fe3293086035a00fb1df3
Yusuf Ghani <66741499+ChampionYusuf1@users.noreply.github.com>
2023-05-31 09:41:41 -0500

    Add files via upload

--- Content.js
+++ Content.js
@@ -1,1 +1,19 @@
-alert("Hey Guys");
+const generateHTML = (pageName) => {
+    return ' <button>STUDYING IS BETTER THEN PROCRASTINATING</button> '
+};
+
+
+switch (window.location.hostname){
+        case "www.youtube.com":
+    document.body.innerHTML = "<button> GO BACK TO STUDYING</button>"
+    case "www.facebook.com":
+        document.body.innerHTML = "<button> GO BACK TO STUDYING</button>"
+        break;
+    case "www.netflix.com":
+        document.body.innerHTML = "<button> GO BACK TO STUDYING</button>"
+        break;
+        case "www.instagram.com":
+            document.body.innerHTML = "<button> GO BACK TO STUDYING</button>"
+            break;
+         
+}
